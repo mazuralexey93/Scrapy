2fef3881436ae5bcb60f00a7b71d5330143d7a02
aleksei@mazur <mazuralexey@gmail.com>
2022-08-04 01:00:02 +0300

    ready to get dirty data

--- jobparser/pipelines.py
+++ jobparser/pipelines.py
@@ -6,8 +6,24 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+from pymongo import MongoClient
 
 class JobparserPipeline:
+    def __init__(self):
+        client = MongoClient('localhost', 27017)
+        self.mongo_base = client.vacancies0408
+
     def process_item(self, item, spider):
+        print()
+        # item['salary'] = self.process_salary(item['salary'])
+
+        collection = self.mongo_base[spider.name]
+        collection.insert_one(item)
+
         return item
+
+    def process_salary(self, salary):
+        min = 0
+        max = 0
+        cur = ''
+        return min, max, cur
--- jobparser/settings.py
+++ jobparser/settings.py
@@ -20,12 +20,12 @@ USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTM
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 16
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 0
+DOWNLOAD_DELAY = 0.5
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -64,9 +64,9 @@ LOG_LEVEL = "DEBUG"
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'jobparser.pipelines.JobparserPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'jobparser.pipelines.JobparserPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
