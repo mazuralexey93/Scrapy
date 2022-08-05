e4de3436944ba261b42504a7fc70aa719c064a9f
aleksei@mazur <mazuralexey@gmail.com>
2022-08-05 03:49:54 +0300

    processong salary

--- jobparser/pipelines.py
+++ jobparser/pipelines.py
@@ -8,22 +8,30 @@
 from itemadapter import ItemAdapter
 from pymongo import MongoClient
 
+
 class JobparserPipeline:
     def __init__(self):
         client = MongoClient('localhost', 27017)
         self.mongo_base = client.vacancies0408
 
     def process_item(self, item, spider):
-        print()
-        # item['salary'] = self.process_salary(item['salary'])
-
+        item['salary'] = self.process_salary(item['salary'])
         collection = self.mongo_base[spider.name]
         collection.insert_one(item)
 
         return item
 
     def process_salary(self, salary):
-        min = 0
-        max = 0
-        cur = ''
-        return min, max, cur
+        salary_min, salary_max, salary_cur = None, None, None
+
+        if len(salary) == 1:
+            ...  # [з/п не указана]
+
+        elif salary[0] == 'от ' and salary[2] == ' до ':
+            salary_min, salary_max, salary_cur = int(salary[1].replace("\xa0", "")), salary[3].replace("\xa0", ""), salary[5]
+        elif salary[0] == 'от ' and salary[0] != ' до ':
+            salary_min, salary_max, salary_cur = salary[1].replace("\xa0", ""), salary_max, salary[3]
+        elif salary[0] == 'до ' and salary[0] != 'от ':
+            salary_min, salary_max, salary_cur = salary_min, int(salary[1].replace("\xa0", "")), salary[3]
+
+        return {'salary_min': salary_min, 'salary_max': salary_max, 'salary_cur': salary_cur}
